4e78c6cdade096af0777acd150dd44aa3d0f3860
Haamthelord1 <94069564+Haamthelord1@users.noreply.github.com>
2022-06-05 21:46:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ TOKEN = "5457489383:AAECKnOy7I1OjmgtEupeBYShieNJVYTjhBk"
 bot = telebot.TeleBot(TOKEN) 
 def send_image():
  a = random.randint(0,1)
- captions = ("🔴 Join Red On Parity \n✋ Always Use /Triple_Investment Method To Be In Profit\n🔸️If You Are New, Register via Official Link :\n https://winapp1.com//#/register?r_code=68C2F335 \n\nAnd Start Earning Now!", "🟢 Join Green On Parity \n✋ Always Use /Triple_Investment Method To Be In Profit\n🔸️If You Are New, Register via Official Link :\n https://winapp1.com//#/register?r_code=68C2F335 \n\nAnd Start Earning Now!) 
+ captions = ("🔴 Join Red On Parity \n✋ Always Use /Triple_Investment Method To Be In Profit\n🔸️If You Are New, Register via Official Link :\n https://winapp1.com//#/register?r_code=68C2F335 \n\nAnd Start Earning Now!", "🟢 Join Green On Parity \n✋ Always Use /Triple_Investment Method To Be In Profit\n🔸️If You Are New, Register via Official Link :\n https://winapp1.com//#/register?r_code=68C2F335 \n\nAnd Start Earning Now!") 
  with open(f"{a}.png", "rb") as file: bot.send_photo("-1001692814230", file,caption[a])
 
 while True:
