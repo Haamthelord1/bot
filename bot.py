a6c49c9537d0755ff3af1dad0ed119e19b1d71a0
Haamthelord1 <94069564+Haamthelord1@users.noreply.github.com>
2022-06-05 21:29:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,12 @@
-import telebot
-import time
-
-TOKEN = "5457489383:AAECKnOy7I1OjmgtEupeBYShieNJVYTjhBk"
-bot = telebot.TeleBot(TOKEN)
-
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    bot.send_message(message.chat.id,message.text)
-
-
-@bot.message_handler(commands=["hello", "hi"])
-def hello(message):
-    #jokes = ["Red","Green"]
-    #randomjoke = random.randrange([jokes])
-    bot.send_message(message.chat.id,"Still In Development 🥲")
-
+import telebot 
+import time 
+TOKEN = "5457489383:AAECKnOy7I1OjmgtEupeBYShieNJVYTjhBk" 
+bot = telebot.TeleBot(TOKEN) 
+def send_image():
+ a = random.randint(0,1)
+ captions = ("red", "green") 
+ with open(f"{a}.png", "rb") as file: bot.send_photo("group_id", file,caption[a])
 
 while True:
-    try:
-        bot.polling()
-    except:
-        time.sleep(5)
+ send_image()
+ time.sleep(10)
