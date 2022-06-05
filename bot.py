4608e751bfa56cd771911b852ea7e3367d2383b7
Haamthelord1 <94069564+Haamthelord1@users.noreply.github.com>
2022-06-05 22:01:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,14 @@
 import telebot 
-
 import time 
-
 import random
-
 TOKEN = "5457489383:AAECKnOy7I1OjmgtEupeBYShieNJVYTjhBk" 
-
 bot = telebot.TeleBot(TOKEN) 
-
 def send_image():
-
  a = random.randint(0,1)
-
  captions = ("🔴 Red", "🟢 Green ") 
-
- with open(f"{a}.png", "rb") as file: bot.send_photo(-1001692814230, file,caption[a])
+ with open(f"{a}.png", "rb") as file:
+     bot.send_photo(-1001692814230, file,caption[a])
 
 while True:
-
  send_image()
-
  time.sleep(10)
